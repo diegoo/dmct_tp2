cc586bd0f6a17e29c23a061fad8faa9ea926a53c
Diego Dell'Era <ddellera@socialmetrix.com>
2015-12-19 19:57:52 -0300

    2

--- tp.md
+++ tp.md
@@ -18,27 +18,35 @@ La relación modelada por cada arista es "X buscó a Y para pedirle consejo prof
 * diámetro:			  5
 * porcentaje de aristas simétricas: 17 %
 * longitud de camino promedio:	  2.33
-* análisis de grados:		
+* análisis de grados:
+
   - plot histograma de grados
 
  [![histograma_grados](https://github.com/diegoo/dmct_tp2/blob/master/histograma_grados_red_consejos.png)](#grados)
 
-  - vértice con mayor grado: observación nro. 17 (16 si 0-indexed), con grado 50.
-    {'status': 1,
-     'office': 1,
-     'gender': 1,
-     'age': 50,
-     'practice': 2,
-     'law_school': 1,
-     'seniority': 23}) => el más consultado es uno de los socios,
-     		       	  hombre de 50 años que estudió en Harvard,
-			  trabaja en la oficina de Boston en derecho
-			  comercial, y está hace 23 años en la firma.
+  - vértices con mayor grado *in*: abogados (0-indexed) a quienes más les piden consejos (grado *in* 29):
+    - 16, {'status': 1, 'office': 1, 'gender': 1, 'age': 50, 'practice': 2, 'law_school': 1, 'seniority': 23}): uno de los socios, hombre de 50 años que estudió en Harvard, trabaja en la oficina de Boston en derecho comercial, y está hace 23 años en la firma.
+    - 25, {'status': 1, 'office': 1, 'gender': 1, 'age': 41, 'practice': 1, 'law_school': 3, 'seniority': 15}): uno de los socios, hombre de 41 años que trabaja en la oficina de Boston en el área de litigios, y está hace 15 años en la firma.
 
-medidas de centralidad:
+  - vértices con mayor grado *out*: abogados (0-indexed) que piden consejos a más personas (grado *out* 27):
+    - 15, {'status': 1, 'office': 1, 'gender': 1, 'age': 46, 'practice': 2, 'law_school': 1, 'seniority': 20}: , hombre de 41 años que estudió en Harvard, trabaja en la oficina de Boston en el área de derecho comercial, y está hace 20 años en la firma.
+
+  - vértices con mayor grado total (*in* + *out*): (grado total 50):
+    - 16 nuevamente
+ 
+* medidas de centralidad:
+
+ [![betweenness_vertices](https://github.com/diegoo/dmct_tp2/blob/master/betweenness_vertices.png.png)](#betweenness_vertices)
+
+  - puntos importantes (0-indexed) con medida de betweenness mucho más alta que los demás: 
+    - 15, 771.88 {'status': 1, 'office': 1, 'gender': 1, 'age': 46, 'practice': 2, 'law_school': 1, 'seniority': 20}) 
+    - 16, 616.86 {'status': 1, 'office': 1, 'gender': 1, 'age': 50, 'practice': 2, 'law_school': 1, 'seniority': 23}) 
+    - 59, 432.08 {'status': 2, 'office': 1, 'gender': 2, 'age': 31, 'practice': 2, 'law_school': 2, 'seniority': 2})  
+    - 11, 339.08 {'status': 1, 'office': 1, 'gender': 1, 'age': 52, 'practice': 2, 'law_school': 2, 'seniority': 24}) 
 
 
 ### 3. Buscar correlaciones entre medidas de centralidad y atributos de los vértices
+
 
 ### 4.
 
--- tp2.py
+++ tp2.py
@@ -5,6 +5,7 @@ from igraph import *
 from lazatt import atributos_de_vertices
 import pylab, matplotlib
 from itertools import izip
+from scipy.stats.stats import pearsonr   
 
 
 ## 1.1 construir grafo ADVICE
@@ -33,8 +34,35 @@ print(red_consejo.is_connected())
 relaciones_mutuas = [x for x in red_consejo.is_mutual() if x is True]
 print(len(relaciones_mutuas) / float(red_consejo.ecount())) # lo mismo: red_consejo.reciprocity()
 print(red_consejo.average_path_length())
-nodo_con_maximo_grado = red_consejo.vs.select(_degree = red_consejo.maxdegree())
-print(nodo_con_maximo_grado.find())
+
+max_grado_in  = max(red_consejo.degree(mode="in"))
+max_grado_out = max(red_consejo.degree(mode="out"))
+max_grado_all = max(red_consejo.degree(mode="all"))
+
+nodos_con_maximo_grado_in = filter(lambda v: v.degree(mode="in") == max_grado_in, red_consejo.vs)
+nodos_con_maximo_grado_out = filter(lambda v: v.degree(mode="out") == max_grado_out, red_consejo.vs)
+nodos_con_maximo_grado_all = filter(lambda v: v.degree(mode="all") == max_grado_all, red_consejo.vs)
+print(list(nodos_con_maximo_grado_in))
+print(list(nodos_con_maximo_grado_out))
+print(list(nodos_con_maximo_grado_all))
+
+# lo mismo:
+# nodo_con_maximo_grado_total = red_consejo.vs.select(_degree = red_consejo.maxdegree())
+# print(nodo_con_maximo_grado_total.find())
+
+
+
+
+
+
+
+
+
+[(a,b) for (a,b) in enumerate(red_consejo.degree(type="in")) if b == red_consejo.maxdegree(type="in")]
+Out[291]: [(16, 29), (25, 29)]
+
+
+
 
 # histograma de grados
 
@@ -45,8 +73,77 @@ pylab.title("grados red ADVICE")
 pylab.bar(xs, ys)
 # pylab.show()
 
+# medidas de centralidad para vértices
+# closeness: distancia relativa a todos los otros vértices
+# betweenness: cantidad de caminos más cortos en los que aparece el vértice
+# eigenvector centrality:
+
+# plotear x betweenness y evcent:
+# low x high y: important gatekeeper to central actor
+# high x low y: actor with exclusive access to central actor
+
+red_consejo_betweenness = red_consejo.betweenness()
+pylab.plot([vertice.index for vertice in red_consejo.vs], red_consejo_betweenness)
+pylab.xlabel(u"vértice")
+pylab.ylabel(u"centralidad")
+pylab.title("medida de centralidad: betweenness")
+#pylab.show()
+
+# vértices que se destacan por alto betweenness
+[(v,b) for (v,b) in enumerate(red_consejo_betweenness) if b > 300]
+
+red_consejo_closeness = red_consejo.closeness()
+pylab.plot([vertice.index for vertice in red_consejo.vs], red_consejo_closeness)
+pylab.xlabel(u"vértice")
+pylab.ylabel(u"centralidad")
+pylab.title("medida de centralidad: closeness")
+#pylab.show()
+
+red_consejo_centralidad_por_autovalor = red_consejo.evcent()
+pylab.plot([vertice.index for vertice in red_consejo.vs], red_consejo_centralidad_por_autovalor)
+pylab.xlabel(u"vértice")
+pylab.ylabel(u"centralidad")
+pylab.title("medida de centralidad: autovalores")
+#pylab.show()
+
+# red_consejo_betweenness_normalizados = [x / 771.885 for x in red_consejo_betweenness]
+# pylab.plot([vertice.index for vertice in red_consejo.vs], [abs(a - b) for (a,b) in zip(red_consejo_betweenness_normalizados, red_consejo_centralidad_por_autovalor)])
+
+vertice_mayor_betweenness = [x for x in red_consejo.vs if red_consejo_betweenness[x.index] == max(red_consejo_betweenness)][0]
+vertice_mayor_closeness = [x for x in red_consejo.vs if red_consejo_closeness[x.index] == max(red_consejo_closeness)][0]
+# en ambos casos: 15,{'status': 1, 'office': 1, 'gender': 1, 'age': 46, 'practice': 2, 'law_school': 1, 'seniority': 20})
+
+
+
 
 ## 3. buscar correlaciones entre medidas de centralidad y atributos de los vértices
+
+# edad
+pylab.hist(red_consejo.vs["age"], bins=35)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["status"]))
+(-0.23732194274061164, 0.046285106427618829)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["gender"]))
+(-0.051815545105643575, 0.66781823077085289)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["age"]))
+(0.16475816855013578, 0.16974028798784357)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["seniority"]))
+(0.21122194054899776, 0.07702662066458045)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["office"]))
+(-0.14797927383206555, 0.21811473766259087)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["status"]))
+(-0.23732194274061164, 0.046285106427618829)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["law_school"]))
+(-0.28393163991364734, 0.016413220954138073)
+
+print(pearsonr(red_consejo_betweenness, red_consejo.vs["practice"]))
+(0.26813510883890224, 0.02377068212044943)
 
 
 ## 4. buscar comunidades
