24223bb10c7756b3026011901c91abdbe4c28fa5
Diego Dell'Era <ddellera@socialmetrix.com>
2015-12-19 14:27:41 -0300

    1,2

---
+++ tp.md
@@ -0,0 +1,44 @@
+## Tareas específicas
+
+### 1. Construir redes
+
+1era red: Los primeros 71 registros de la tabla original. Es la red "ADVICE".
+La relación modelada por cada arista es "X buscó a Y para pedirle consejo profesional".
+
+### 2. Caracterizar topología de las redes
+
+1era red
+
+* tipo de red:			  grafo dirigido
+* ¿conectado?:			  no
+* ¿simple?:			  sí
+* número de vértices: 		  71
+* número de aristas: 		  612 
+* densidad: 			  0.12313883299798792
+* diámetro:			  5
+* porcentaje de aristas simétricas: 17 %
+* longitud de camino promedio:	  2.33
+* análisis de grados:		
+  - plot histograma de grados
+ 
+  - vértice con mayor grado: observación nro. 17 (16 si 0-indexed), con grado 50.
+    {'status': 1,
+     'office': 1,
+     'gender': 1,
+     'age': 50,
+     'practice': 2,
+     'law_school': 1,
+     'seniority': 23}) => el más consultado es uno de los socios,
+     		       	  hombre de 50 años que estudió en Harvard,
+			  trabaja en la oficina de Boston en derecho
+			  comercial, y está hace 23 años en la firma.
+
+medidas de centralidad:
+
+
+### 3. Buscar correlaciones entre medidas de centralidad y atributos de los vértices
+
+### 4.
+
+### 5.
+
--- tp2.py
+++ tp2.py
@@ -3,18 +3,55 @@
 
 from igraph import *
 from lazatt import atributos_de_vertices
+import pylab, matplotlib
+from itertools import izip
 
-red_coworkers = Graph.Read_Adjacency("lazega.red.coworkers.data")
 
-red_coworkers.vs["status"] =      atributos_de_vertices["status"]  
-red_coworkers.vs["gender"] =      atributos_de_vertices["gender"]  
-red_coworkers.vs["office"] =      atributos_de_vertices["office"]  
-red_coworkers.vs["seniority"] =   atributos_de_vertices["seniority"] 
-red_coworkers.vs["age"] =         atributos_de_vertices["age"]     
-red_coworkers.vs["practice"] =    atributos_de_vertices["practice"]
-red_coworkers.vs["law_school"] =  atributos_de_vertices["law_school"]
+## 1.1 construir grafo ADVICE
 
-# plot(red_coworkers)
+red_consejo = Graph.Read_Adjacency("lazega.red.consejo.data", mode=ADJ_DIRECTED))
+# plot(red_consejo)
+
+red_consejo.vs["status"] =      atributos_de_vertices["status"]  
+red_consejo.vs["gender"] =      atributos_de_vertices["gender"]  
+red_consejo.vs["office"] =      atributos_de_vertices["office"]  
+red_consejo.vs["seniority"] =   atributos_de_vertices["seniority"] 
+red_consejo.vs["age"] =         atributos_de_vertices["age"]     
+red_consejo.vs["practice"] =    atributos_de_vertices["practice"]
+red_consejo.vs["law_school"] =  atributos_de_vertices["law_school"]
+
+# plot(red_consejo)
+
+## 2. caracterización topológica
+
+print(red_consejo.ecount())
+print(red_consejo.vcount())
+print(red_consejo.diameter())
+print(red_consejo.is_directed())
+print(red_consejo.is_simple())
+print(red_consejo.is_connected())
+relaciones_mutuas = [x for x in red_consejo.is_mutual() if x is True]
+print(len(relaciones_mutuas) / float(red_consejo.ecount())) # lo mismo: red_consejo.reciprocity()
+print(red_consejo.average_path_length())
+nodo_con_maximo_grado = red_consejo.vs.select(_degree = red_consejo.maxdegree())
+print(nodo_con_maximo_grado.find())
+
+# histograma de grados
+
+xs, ys = zip(*[(left, count) for left, _, count in red_consejo.degree_distribution().bins()])
+pylab.xlabel("grado")
+pylab.ylabel(u"cantidad de vértices")
+pylab.title("grados red ADVICE")
+pylab.bar(xs, ys)
+# pylab.show()
+
+
+## 3. buscar correlaciones entre medidas de centralidad y atributos de los vértices
+
+
+## 4. buscar comunidades
+
+# armar comunidades con varios métodos
 
 funciones_deteccion_comunidad = [
     Graph.community_edge_betweenness,
@@ -24,34 +61,14 @@ funciones_deteccion_comunidad = [
     Graph.community_leading_eigenvector
 ]
 
-red_coworkers_comunidades = [f(red_coworkers) for f in funciones_deteccion_comunidad]
+red_consejo_comunidades_por_metodo = [f(red_consejo) for f in funciones_deteccion_comunidad]
 
-for comunidad in red_coworkers_comunidades: print summary(comunidad)
+for comunidades in red_consejo_comunidades_por_metodo:
+    print summary(comunidad)
+    n = communities.optimal_count
+    communities.as_clustering(num_communities)
 
-
-
-
-
-
-
-    
-num_communities = communities.optimal_count
-communities.as_clustering(num_communities)
-
-# calculate dendrogram
-dendrogram = graph.community_edge_betweenness()
-
-# convert it into a flat clustering
-clusters = dendrogram.as_clustering()
-
-# get the membership vector
-membership = clusters.membership
-
-# Now we can start writing the membership vector along with the node names into a CSV file::
-
-import csv
-from itertools import izip
-
-writer = csv.writer(open("output.csv", "wb"))
-for name, membership in izip(graph.vs["name"], membership):
-    writer.writerow([name, membership])
+red_consejo_clusters = red_consejo.community_edge_betweenness().as_clustering()
+membresias = red_consejo_clusters.membership
+for observacion, membresia in izip(red_consejo, membresias):
+    print(observacion, membresias)
