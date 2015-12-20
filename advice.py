5de957d447007038a46b1782db11f8cd3ca67ca8
Diego Dell'Era <ddellera@socialmetrix.com>
2015-12-20 19:03:38 -0300

    advice.py

--- advice.py
+++ advice.py
@@ -8,7 +8,7 @@ from itertools import izip
 from scipy.stats.stats import pearsonr   
 from itertools import groupby
 
-## 1.1 construir grafo ADVICE
+## 1. construir grafo ADVICE
 
 red_consejo = Graph.Read_Adjacency("lazega.red.consejo.data", mode=ADJ_DIRECTED)
 # plot(red_consejo)
@@ -110,6 +110,7 @@ vertice_mayor_centralidad_por_autovector = [x for x in red_consejo.vs if red_con
 
 # edad
 pylab.hist(red_consejo.vs["age"], bins=35)
+# pylab.show()
 
 print(pearsonr(red_consejo_betweenness, red_consejo.vs["status"]))
 print(pearsonr(red_consejo_betweenness, red_consejo.vs["gender"]))
@@ -119,14 +120,6 @@ print(pearsonr(red_consejo_betweenness, red_consejo.vs["office"]))
 print(pearsonr(red_consejo_betweenness, red_consejo.vs["law_school"]))
 print(pearsonr(red_consejo_betweenness, red_consejo.vs["practice"]))
 
-# (-0.23732194274061164, 0.046285106427618829)
-# (-0.051815545105643575, 0.66781823077085289)
-# (0.16475816855013578, 0.16974028798784357)
-# (0.21122194054899776, 0.07702662066458045)
-# (-0.14797927383206555, 0.21811473766259087)
-# (-0.28393163991364734, 0.016413220954138073)
-# (0.26813510883890224, 0.02377068212044943)
-
 print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["status"]))
 print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["gender"]))
 print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["age"]))
@@ -134,14 +127,6 @@ print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["seniority
 print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["office"]))
 print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["law_school"]))
 print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["practice"]))
-
-# (-0.68275628890445883, 5.4414842455456928e-11)
-# (-0.20988967839008382, 0.078952569368606479)
-# (0.52825402910516117, 2.1936856999030843e-06)
-# (0.64867071910399554, 9.5307252679818568e-10)
-# (-0.20681140897402797, 0.083549177399459204)
-# (-0.40234844288785476, 0.00050458116868182961)
-# (0.16085302776512134, 0.18022634794134504)
 
 
 ## 4. buscar comunidades
@@ -154,21 +139,20 @@ print(pearsonr(red_consejo_centralidad_por_autovector, red_consejo.vs["practice"
 # Graph.community_infomap
 # Graph.community_leading_eigenvector
 
-comunidades = red_consejo.community_edge_betweenness()
-clusters = comunidades.as_clustering()
-for cluster, vertices in groupby(clusters, lambda x: x[0]): print(cluster, list(vertices))
-# (1, [[1, 7, 10, 12, 20, 23, 25, 26, 28, 33, 35, 37, 38, 40, 42, 51, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 69, 70]])
-# (24, [[24, 31, 49, 52, 68]])
+comunidades_eb = red_consejo.community_edge_betweenness()
+clusters_eb = comunidades_eb.as_clustering()
+# for cluster, vertices in groupby(clusters_eb lambda x: x[0]): print(cluster, list(vertices))
+# cluster_eb_0 = [v for v in red_consejo.vs if v.index in [1, 7, 10, 12, 20, 23, 25, 26, 28, 33, 35, 37, 38, 40, 42, 51, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 69, 70]]
+# cluster_eb_1 = [v for v in red_consejo.vs if v.index in [24, 31, 49, 52, 68]]
 # el resto, clusters con 1 elemento
-cluster1 = [v for v in red_consejo.vs if v.index in [1, 7, 10, 12, 20, 23, 25, 26, 28, 33, 35, 37, 38, 40, 42, 51, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 69, 70]]
-cluster24 = [v for v in red_consejo.vs if v.index in [24, 31, 49, 52, 68]]
 
-comunidades = red_consejo.community_label_propagation()
-clusters_con_vertices = sorted([(cluster, vertice) for (vertice, cluster) in zip(range(0, 71), comunidades.membership)])
-for cluster, vertices in groupby(clusters_con_vertices, lambda x: x[0]): print(cluster, len(list(vertices)))
+comunidades_lp = red_consejo.community_label_propagation()
+clusters_lp = sorted([(cluster, vertice) for (vertice, cluster) in zip(range(0, 71), comunidades_lp.membership)])
+# for cluster, vertices in groupby(clusters_lp, lambda x: x[0]): print(cluster, len(list(vertices)))
 
 comunidades = red_consejo.community_walktrap()
 clusters = comunidades.as_clustering()
+
 print(clusters)
 # Clustering with 71 elements and 11 clusters
 # [ 0] 0, 1, 3, 6, 7, 9, 11, 13, 14, 15, 16, 18, 19, 21, 22, 27, 29, 32, 34, 36, 43
@@ -183,15 +167,17 @@ print(clusters)
 # [ 9] 63, 70
 # [10] 64
 
-cluster0 = [v for v in red_consejo.vs if v.index in clusters[0]]
-cluster1 = [v for v in red_consejo.vs if v.index in clusters[1]]
-cluster2 = [v for v in red_consejo.vs if v.index in clusters[2]]
-cluster3 = [v for v in red_consejo.vs if v.index in clusters[3]]
-cluster4 = [v for v in red_consejo.vs if v.index in clusters[4]]
+cluster0 = [vertice for vertice in red_consejo.vs if vertice.index in clusters[0]]
+cluster1 = [vertice for vertice in red_consejo.vs if vertice.index in clusters[1]]
+cluster2 = [vertice for vertice in red_consejo.vs if vertice.index in clusters[2]]
+cluster3 = [vertice for vertice in red_consejo.vs if vertice.index in clusters[3]]
+cluster4 = [vertice for vertice in red_consejo.vs if vertice.index in clusters[4]]
 
-max(clusters.membership)
-# 10
+# ver grado de los vértices en los clusters principales
+for cluster in [cluster0, cluster1, cluster2, cluster3, cluster4]:
+    for vertice in cluster: print vertice, vertice.degree(mode="in"), vertice.degree(mode="out")
 
+# plotear clusters (comunidades)
 color_list = [
     'red',    # cluster 0
     'blue',   # cluster 1
@@ -205,26 +191,5 @@ color_list = [
     'black',
     'purple'
 ]
+# plot(red_consejo, "walktrap_clusters.png", layout="kk", vertex_color=[color_list[x] for x in clusters.membership], vertex_size=5)
 
-plot(red_consejo, "walktrap.png", layout="kk", vertex_color=[color_list[x] for x in clusters.membership], vertex_size=5)
-
-# ver grado en los clusters principales
-for c in [cluster0, cluster2, cluster8]:
-    for v in c: print v, v.degree(mode="in"), v.degree(mode="out")
-
-
-
-
-
-
-comunidades_por_infomap = Graph.community_infomap(red_consejo)
-
-for comunidades in [comunidades_por_infomap, comunidades_por_edge_betweenness]:
-    summary(comunidades)
-    n = comunidades.optimal_count
-    communities.as_clustering(num_communities)
-
-red_consejo_clusters = red_consejo.community_edge_betweenness().as_clustering()
-membresias = red_consejo_clusters.membership
-for observacion, membresia in izip(red_consejo, membresias):
-    print(observacion, membresias)
